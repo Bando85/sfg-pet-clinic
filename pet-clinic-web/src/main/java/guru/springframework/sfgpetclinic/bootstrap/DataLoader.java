48477e3882a1b4621c5f877ee2c5db3bfebf07ea
Bando85 <laczoandras@gmail.com>
2019-12-18 23:33:04 +0100

    impl map based service, closes #6

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Andras Laczo 2019. 12. 18.
+ */
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
