71cc87486df10661f6ec3e175f0563d229079826
Bando85 <laczoandras@gmail.com>
2020-01-19 10:12:15 +0100

    added visits, updated owner with pet relationship. closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,21 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Andras Laczo 2019. 12. 05.
  */
 
 public class Owner extends Person {
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    private Set<Pet> pets;
+
 }
