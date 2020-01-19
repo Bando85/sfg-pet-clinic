ff38f991c03349f2e7f338ff69f42f44a71c36b4
Bando85 <laczoandras@gmail.com>
2020-01-19 13:13:36 +0100

    added PetTypeService closes #30

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by Andras Laczo 2020. 01. 19.
+ */
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
