14fe8cdc7547344c4df1405d86b20cd0252ae9b6
Bando85 <laczoandras@gmail.com>
2020-02-02 20:49:32 +0100

    updated profiles for JPA service, tested active profiles. Closes #49.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -14,7 +14,7 @@ import java.util.Set;
  */
 
 @Service
-@Profile("srpingdatajpa")
+@Profile("springdatajpa")
 public class PetTypeSDJpaService implements PetTypeService {
 
     private final PetTypeRepository petTypeRepository;
